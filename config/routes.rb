0a2309878e9e920cef9f5a49f10a9c83685ce89e
manchere <manucheremeh1995@gmail.com>
2020-02-03 00:10:24 +0000

    Task 1

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  root 'users#index'
+  # get('/users/:id', to: 'users#show', as 'user')
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
